5faa2c58985fd992e2dbc43f371ce278279ba1b9
Aditya Kumar <adityakumar5155@gmail.com>
2025-03-09 13:57:25 +0530

    Readded Event Manager Button

--- client/src/components/Landing_Page_partials/Header.jsx
+++ client/src/components/Landing_Page_partials/Header.jsx
@@ -8,7 +8,21 @@ function Header() {
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
                 <div className="flex items-center justify-between h-20">
                     {/* Desktop navigation */}
-                    
+                    <nav className="flex grow">
+                        {/* Desktop sign in links */}
+                        <ul className="flex grow justify-end flex-wrap items-center">
+                            <li>
+                                <a
+                                    onClick={() =>
+                                        router.push("/admin/auth")
+                                    }
+                                    className="btn-sm text-white  text- bg-[#38BDF8] hover:bg-gray-700 w-full sm:w-auto  sm:ml-3 cursor-pointer"
+                                >
+                                    Event Manager
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
